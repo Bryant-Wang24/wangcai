897457cae1725a3b15bdbb1a298bdbbb60c2ebb1
wangqiushuang <485969746@qq.com>
2020-10-03 13:21:03 +0800

    first commit

---
+++ src/components/Money/FormItem.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+    <label class="formItem">
+        <span class="name">{{this.fieldName}}</span>
+        <input type="text" v-model="value" :placeholder="this.placeholder">
+    </label>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {
+    Component,
+    Prop,
+    Watch
+} from 'vue-property-decorator';
+@Component
+export default class FormItem extends Vue {
+    @Prop({
+        default: ''
+    }) value!: string;
+    @Prop({
+        required: true
+    }) fieldName!: string;
+    // eslint-disable-next-line @typescript-eslint/type-annotation-spacing
+    @Prop() placeholder ? : string;
+    @Watch('value')
+    onValueChange(value: string) {
+        this.$emit('update:value', value);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.formItem {
+    font-size: 14px;
+    background: #f5f5f5;
+    padding-left: 16px;
+    display: flex;
+    align-items: center;
+
+    .name {
+        padding-right: 16px;
+    }
+
+    input {
+        height: 40px;
+        flex-grow: 1;
+        background: transparent;
+        border: none;
+        padding-right: 16px;
+    }
+}
+</style>
