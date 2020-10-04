ef4969ac491edd6224b7111acc76a97c2c7dd582
wangqiushuang <485969746@qq.com>
2020-10-04 22:44:17 +0800

    Labels.vue 使用 Vuex，抽离 TagHelper 作为 mixin

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,20 +18,24 @@ import {
     Component
 } from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
+import {
+    mixins
+} from 'vue-class-component';
+import TagHelper from '@/mixins/TagHelper';
 
 @Component({
     components: {
         Button
+    },
+    computed: {
+        tags() {
+            return this.$store.state.tagList;
+        },
     }
 })
-export default class Labels extends Vue {
-    tags = [];
-
-    createTag() {
-        const name = window.prompt('请输入标签名');
-        if (name) {
-            // store.createTag(name);
-        }
+export default class Labels extends mixins(TagHelper) {
+    beforeCreate() {
+        this.$store.commit('fetchTags');
     }
 }
 </script>
